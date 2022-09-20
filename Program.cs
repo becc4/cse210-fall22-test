3904f21801c1860cadcdd2597388c278da0f691d
Becca Roeth <roe21002@byui.edu>
2022-09-20 15:54:04 -0600

    Adding everything to file

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+// See https://aka.ms/new-console-template for more information
+namespace Test
+{
+    public class Program
+    {
+        public static void Main(string[] arg)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+};
+
+// Console.WriteLine("Hello, World!");
